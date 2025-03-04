4121320eed2215e9598dcce8bf0d9fa4258719d7
Manh Nguyen <manhxnguyen11@gmail.com>
2025-03-03 22:09:06 -0600

    update new api url

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3002/api/v1';
+const API_URL = 'https://task-list-mo6j.onrender.com/api/v1/';
 
 export const taskService = {
   getTasks: async (filter) => {
